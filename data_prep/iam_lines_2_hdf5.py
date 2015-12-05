b6c846ff602b8b98631de14c801090d241682883
Yonas Tesfaye <ymt123@alum.mit.edu>
2015-12-04 16:24:10 -0800

    Extract lines from full forms using data from the lines.txt file

--- data_prep/iam_lines_2_hdf5.py
+++ data_prep/iam_lines_2_hdf5.py
@@ -2,50 +2,75 @@ import glob
 import h5py
 import sys
 import os
-
+from collections import defaultdict, namedtuple
+import numpy as np
 import scipy.misc
+
 from utils import *
 
-def process_file(input_path):
-    pass
-    # Read line
-    # return
+bbox = namedtuple('bbox', ['min_x', 'min_y', 'max_x', 'max_y'])
 
-def process_folder(input_path, output_fname):
+def get_form_info(fname):
+    line_info = defaultdict(list)
+    for line in open(fname):
+        if not line.startswith('#'):
+            ls = line.strip().split()
+            line_name = ls[0].split('-')
+            line_info['-'.join(line_name[:2])].append(ls)
+
+    X = 4
+    Y = 5
+    W = 6
+    H = 7
+    form_info = defaultdict(list)
+    for form in line_info:
+        max_x = None
+        min_x = None
+        max_y = None
+        min_y = None
+        for line in line_info[form]:
+            x = int(line[X])
+            y = int(line[Y])
+            w = int(line[W])
+            h = int(line[H])
+            form_info[form].append(bbox(x, y, x+w, y+h))
+    return form_info
+
+
+def process_folder(input_path, output_fname, form_info):
     '''
     Takes input data, normalizes lines and saves to hdf5 (assumes data is organized as input_path/author/form/line#.png)
     '''
 
     # Caculate which files we need to look at
-    png_files = glob.glob(os.path.join(input_path, '*', '*', '*.png'))
+    png_files = glob.glob(os.path.join(input_path, '*.png'))
     split_files = []
     for png_file in png_files:
-        (path, line_name) = os.path.split(png_file)
-        (path, form_name) = os.path.split(path)
-        (path, author) = os.path.split(path)
-        split_files.append((author, form_name, line_name, png_file))
-
+        form_name = os.path.basename(png_file).split('.')[0]
+        author = form_name.split('-')[0]
+        split_files.append((author, form_name, png_file))
 
     # Open Output File
     fOut = h5py.File(output_fname, 'w')
 
     seen_authors = {}
-    seen_forms = {}
     num_items = len(split_files)
     count = 0
-    for (author, form_name, line_name, full_path) in split_files:
+    for (author, form_name, full_path) in split_files:
         if author not in seen_authors:
             author_grp = fOut.create_group(author)
             seen_authors[author] = author_grp
 
-        if form_name not in seen_forms:
-            author_grp = seen_authors[author]
-            form_grp = author_grp.create_group(form_name)
-            seen_forms[form_name] = form_grp
+        author_grp = seen_authors[author]
+        form_grp = author_grp.create_group(form_name)
 
         input_line = scipy.misc.imread(full_path)
-        resized_data = normalize_line(input_line)
-        form_grp.create_dataset(line_name, data=resized_data)
+        for i,line_bbox in enumerate(form_info[form_name]):
+            data_line = input_line[line_bbox.min_y:line_bbox.max_y, line_bbox.min_x:line_bbox.max_x]
+            resized_line = normalize_line(data_line)
+            line_name = '%s-%d.png'%(form_name, i)
+            form_grp.create_dataset(line_name, data=resized_line.astype(np.uint8))
+
         count += 1
 
         if count %100 == 0:
@@ -54,13 +79,15 @@ def process_folder(input_path, output_fname):
     fOut.close()
 
 def main():
-    if len(sys.argv) != 2:
-        print "Usage: python iam_lines_2_hdf5.py /path/to/line_pngs/ /path/to/output_fname.hdf5"
+    if len(sys.argv) != 4:
+        print "Usage: python iam_forms_2_hdf5.py lines.txt /path/to/line_pngs/ /path/to/output_fname.hdf5"
         sys.exit(1)
 
-    input_directory = sys.argv[1]
-    output_hdf5_fname = sys.argv[2]
-    process_folder(input_directory, output_hdf5_fname)
+    line_fname = sys.argv[1]
+    input_directory = sys.argv[2]
+    output_hdf5_fname = sys.argv[3]
+    form_info = get_form_info(line_fname)
+    process_folder(input_directory, output_hdf5_fname, form_info)
 
 if __name__ == "__main__":
     main()
