82d0f8d0d24cc1ea89685e7f039b770569a2e506
kni <kni@iqt.org>
2015-12-28 02:45:31 +0000

    Dense2Convolutional requires transpose first, then reshape, then fliplr & flipud

--- feat_extract/sandbox.py
+++ feat_extract/sandbox.py
@@ -1,34 +1,81 @@
 if False:
     execfile('extractfeats.py')
+#    model = fielnet('../convnets/fielnet/fielnet.hdf5')
 
 if False:
-    print "Compiling all convolutional model, without dense layers"
-    modelconvs = Sequential()
-    for i in xrange(15):
-        modelconvs.add( model.layers[i] )
-    modelconvs.compile( loss='mse', optimizer='sgd' )
-    f_convs = modelconvs.predict(X_test)
+    wholeimname = '/fileserver/nmec-handwriting/stil-writing-corpus/French/French-Images/FR-092-006.tif'
+    wholeimname = '/fileserver/iam/forms/d06-082.png'
+    wholeim = smi.imread(wholeimname)
+    wholeim = 1.0 - wholeim / 255.0
+    wholeim = np.expand_dims(wholeim,0)
+    wholeim = np.expand_dims(wholeim,0)
 
 if False:
-    print "Compiling all convolutional model, with dense layers"
-    model1dense = Sequential()
-    for i in xrange(17):
-        model1dense.add( model.layers[i] )
-    model1dense.compile( loss='mse', optimizer='sgd' )
-    f_1dense= model1dense.predict(X_test)
+    modelc = Sequential()
+    modelc.add( Convolution2D( 4, 12, 12, border_mode='valid', input_shape=(1,120,120)) )
+    modelc.add( MaxPooling2D( pool_size=(4,4) ) )
+    modelc.add( Convolution2D( 5, 27, 27, border_mode='valid') )
+    
+    modeld = Sequential()
+    modeld.add( Convolution2D( 4, 12, 12, border_mode='valid', input_shape=(1,120,120)) )
+    modeld.add( MaxPooling2D( pool_size=(4,4) ) )
+    modeld.add( Flatten() )
+    modeld.add( Dense( 5 ) )
+
+    modelc.compile(loss='mse',optimizer='sgd')
+    modeld.compile(loss='mse',optimizer='sgd')
 
 if False:
-    dense_weights = model.layers[16].get_weights()
-    modeld2c = Sequential()
-    for i in xrange(15):
-        modeld2c.add( model.layers[i] )
-    modeld2c.add( Convolution2D( 128, 10, 10, border_mode = 'valid' ) )
-    modeld2c.add( Activation('relu') )    
-    modeld2c.predict( X_test )
+    modelcw = modelc.layers[0].get_weights()
+    modeld.layers[0].set_weights(modelcw)
+    modelcw = modelc.layers[-1].get_weights()
+    Wc1 = modelcw[0]
 
-wholeimname = '/fileserver/nmec-handwriting/stil-writing-corpus/French/French-Images/FR-092-006.tif'
-wholeimname = '/fileserver/iam/forms/d06-082.png'
-wholeim = smi.imread(wholeimname)
-wholeim = 1.0 - wholeim / 255.0
-wholeim = np.expand_dims(wholeim,0)
-wholeim = np.expand_dims(wholeim,0)
+if True:
+    modeldw = modeld.layers[-1].get_weights()
+    Wd1 = modeldw[0]
+    # Wd1 = Wd1.T.reshape( 5, 4, 27, 27 )
+    # Wd1 = Wd1.transpose(0,3,1,2)
+    # Wd1 = Wd1.transpose(0, 1, 3, 2)
+
+    # modelc.layers[-1].set_weights( [Wd1, modeldw[1]] )
+
+c = modelc.predict(X_test).squeeze()
+d = modeld.predict(X_test).squeeze()
+
+from itertools import permutations
+
+finalshape = [5,4,27,27]
+newtranspose = [0,1,2,3]
+for Wdt in [Wd1, Wd1.T]:
+  for permutation in permutations(range(4)):
+    newshape = [ finalshape[ p ] for p in permutation ]
+    for i,p in enumerate(permutation):
+      newtranspose[p] = i
+    Wdn = Wdt.reshape( *newshape )
+    print "Before transpose "+str(Wdn.shape)+" Reperm: "+str(newtranspose)
+    Wdn = Wdn.transpose( *newtranspose )
+
+    modelc.layers[-1].set_weights( [Wdn, modeldw[1]] )
+    cn = modelc.predict(X_test).squeeze()
+
+    eps = 0.03
+    aggregate_error = np.mean( np.square( cn - d ) )
+    print "Aggregate Error: "+str(aggregate_error)
+    if cn[0,0] <= d[0,0]+eps and cn[0,0] >= d[0,0]-eps:
+      print "Success!"
+      break
+  if cn[0,0] <= d[0,0]+eps and cn[0,0] >= d[0,0]-eps:
+    print "Success!"
+    break
+
+modele = Sequential()
+modele.add( modelc.layers[0] )
+modelf = Sequential()
+modelf.add(  modeld.layers[0] )
+modele.compile( loss='mse', optimizer='sgd')
+modelf.compile( loss='mse', optimizer='sgd')
+
+
+
+
