6aedb3979a118fb1be9e1070b145ae2fffaeb119
pcallier <pcallier@iqt.org>
2016-02-10 21:27:16 +0000

    Use 15.04 in image; disable CNMem by default

--- dockers/keras-cuda-notebook/nv352.79/1-build.sh
+++ dockers/keras-cuda-notebook/nv352.79/1-build.sh
@@ -6,7 +6,10 @@ __image=lab41/cudnn-keras-notebook
 __tag=352.79
 __dockerfile=Dockerfile
 
+cp $FSERVER/users/pcallier/cudnn-7.0-linux-x64-v4.0-rc.tgz .
+
 # build image
 echo "Building $__image:$__tag"
 docker build -f $__dockerfile -t $__image:$__tag .
 
+rm cudnn-7.0-linux-x64-v4.0-rc.tgz
