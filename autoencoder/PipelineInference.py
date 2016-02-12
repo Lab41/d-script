420ced14f9a37b25da7735d208bbf45973f93012
kni <kni@iqt.org>
2016-02-12 23:08:29 +0000

    Ingest Lines

--- autoencoder/PipelineInference.py
+++ autoencoder/PipelineInference.py
@@ -4,19 +4,16 @@ import sys
 import logging
 sys.path.append('..')
 
-#pdb
-
 # Neural network stuff
 from fielutil import load_verbatimnet
-from featextractor import extract_imfeats
+from demo_pipeline.featextractor import extract_imfeats
 
+#pdb
 # Logging
 # logging.getLogger('featextractor').setLevel(logging.DEBUG)
+shingle_dims=(120,120)
 
 # ### Parameters
-
-# In[2]:
-
 # Do you want to load the features in? Or save them to a file?
 load_features = False
 
@@ -24,31 +21,28 @@ load_features = False
 # hdf5images='icdar13data/benchmarking-processed/icdar_be.hdf5'
 # hdf5images = 'icdar13data/experimental-processed/icdar13_ex.hdf5'
 # hdf5images='nmecdata/nmec_scaled_flat.hdf5'
-hdf5images='/fileserver/nmec-handwriting/flat_nmec_bin_uint8.hdf5'
+hdf5images='/fileserver/nmec-handwriting/flat_nmec_cropped_bin_uint8.hdf5'
 
 # This is the file that you will load the features from or save the features to
 # featurefile = 'icdar13data/benchmarking-processed/icdar13be_fiel657.npy'
 # featurefile = 'icdar13data/experimental-processed/icdar13ex_fiel657.npy'
-featurefile = '/fileserver/nmec-handwriting/nmec_bw.deNN_fiel657.step5_noE.npy'
+# featurefile = '/fileserver/nmec-handwriting/nmec_bw.deNN_fiel657.step5_noE.npy'
+# featurefile = '/fileserver/nmec-handwriting/nmec_bw_crop.deNN_fiel657.step5_250.npy'
+featurefile = 'nmec_bw_crop.fiel657_120.step20_250.npy'
 
 # This is the neural networks and parameters you are deciding to use
-paramsfile = '/fileserver/iam/iam-processed/models/fiel_657.hdf5'
+# paramsfile = '/fileserver/iam/iam-processed/models/fiel_657.hdf5'
+paramsfile = 'fielnet120-nmec.hdf5'
 
 
 # ### Full image HDF5 file
 # 
 # Each entry in the HDF5 file is a full image/form/document
-
-# In[3]:
-
 labels = h5py.File(hdf5images).keys()
 
 
 # ### Load feature extractor neural network
-
-# In[ ]:
-
-vnet = load_verbatimnet( 'fc7', paramsfile=paramsfile )
+vnet = load_verbatimnet( 'fc7', input_shape=(1,)+shingle_dims, paramsfile=paramsfile )
 vnet.compile(loss='mse', optimizer='sgd')
 print "Finished loading neural network in and compilation"
 
@@ -56,37 +50,28 @@ print "Finished loading neural network in and compilation"
 # ### Image features
 # 
 # Currently taken as averages of all shard features in the image. You can either load them or extract everything manually, depending on if you have the .npy array.
-
-# In[ ]:
-
 if load_features:
     print "Loading features in from "+featurefile
     imfeats = np.load(featurefile)
     print "Loaded features"
 else:
     print "Begin extracting features from "+hdf5images
-    imfeats = extract_imfeats( hdf5images, vnet, steps=(5,5), varthresh=None )
+    imfeats = extract_imfeats( hdf5images, vnet, shingle_dims=shingle_dims, steps=(20,20), compthresh=250 )
     print h5py.File(hdf5images).keys()
     np.save( featurefile, imfeats )
 
 
 # ### Build classifier
-
-# In[ ]:
-
 imfeats = ( imfeats.T / np.linalg.norm( imfeats, axis=1 ) ).T
 F = imfeats.dot(imfeats.T)
 np.fill_diagonal( F , -1 )
 
 
 # ### Evaluate classifier on HDF5 file (ICDAR 2013)
-
-# In[ ]:
-
 # Top k (soft criteria)
 k = 10
 # Max top (hard criteria)
-maxtop = 2
+maxtop = 3
 # Number of examples per image
 g = 8
 
@@ -95,13 +80,17 @@ softcorrect = 0
 hardcorrect = 0
 totalnum = 0
 for j, i in enumerate(F):
+    if (g+1)%8 == 0:
+        continue
     topk = i.argsort()[-k:]
     # Soft criteria
     if j/g in topk/g:
         softcorrect += 1
     totalnum +=1
     # Hard criteria
-    hardcorrect+= sum([1 for jj in (j/g == topk[-maxtop:]/g) if jj])
+    hardindivid = sum([1 for jj in (j/g == topk[-maxtop:]/g) if jj])
+    if hardindivid == maxtop:
+        hardcorrect += 1
     
 # Print out results    
 print "Top %d (soft criteria) = %f" %( k, (softcorrect+0.0) / totalnum )
