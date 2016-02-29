837b2604dc649f0f0938e5ca3f93595ddd552dce
pcallier <pcallier@iqt.org>
2016-02-29 18:24:32 +0000

    Set container name with environment variable

--- dockers/keras-cudnn-nvidia352.79/3-run-script.sh
+++ dockers/keras-cudnn-nvidia352.79/3-run-script.sh
@@ -14,11 +14,14 @@ __script_path=$(readlink -f $2)
 __command=/opt/script
 __command=$2
 
+
 echo $__command
+docker rm $CONTAINER || true
 # run image
 docker run -ti \
         --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
         --device /dev/nvidia0:/dev/nvidia0 \
+        --name $CONTAINER \
         --volume=$__volume_host:$__volume_cntr \
         --volume=/data/fs4/datasets/:$__volume_data \
         --volume=/dev/shm:/memory \
