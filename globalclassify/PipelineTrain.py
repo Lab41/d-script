0534806bd6cc06f261d851d4eaca05235bbe6f1f
pcallier <pcallier@iqt.org>
2016-02-29 18:23:02 +0000

    Switch back to original PipelineTrain script (IAM version still in commit history)

--- globalclassify/PipelineTrain.py
+++ globalclassify/PipelineTrain.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-
-import time
-from functools import partial
 import numpy as np
 import h5py
 import sys
@@ -10,15 +6,14 @@ sys.path.append('../')
 
 # Neural network stuff
 import data_iters
-from data_iters.hdf5_iterator import Hdf5GetterBatcher
+from data_iters.hdf5_iterator import Hdf5MiniBatcher
 from data_iters.minibatcher import MiniBatcher
 from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
 from keras.utils.np_utils import to_categorical
-from fielutil import load_verbatimnet, patnet_layers
-from preprocessing.segmentation import original_connected
+from fielutil import load_verbatimnet
 
-#from data_iters.archive.iam_iterator import IAM_MiniBatcher
+from data_iters.archive.iam_iterator import IAM_MiniBatcher
 
 
 # ### File names
@@ -31,8 +26,8 @@ from preprocessing.segmentation import original_connected
 
 
 # Which training dataset do we want to train from?
-train_dataset='iam-forms'
-#train_dataset='nmec'
+train_dataset='iam-words'
+train_dataset='nmec'
 
 
 # Do you want to load the features in? Or save them to a file?
@@ -46,9 +41,6 @@ if train_dataset=='nmec':
     hdf5images='nmecdata/nmec_scaled_flat.hdf5'
 elif train_dataset=='iam-words':
     hdf5authors='/fileserver/iam/iam-processed/words/author_words.hdf5'
-elif train_dataset=='iam-forms':
-    hdf5authors='/fileserver/iam/iam-processed/forms/author_forms.hdf5'
-    hdf5images='/fileserver/iam/iam-processed/forms/forms.hdf5'
 elif train_dataset=='iam-lines':
     hdf5authors='/fileserver/iam/iam-processed/lines/author_lines.hdf5'
 else:
@@ -57,10 +49,10 @@ else:
 # This is the file that you will load the features from or save the features to
 # featurefile = 'icdar13data/benchmarking-processed/icdar13be_fiel657.npy'
 # featurefile = 'icdar13data/experimental-processed/icdar13ex_fiel657.npy'
-#featurefile = 'nmecdata/nmec_fiel657_features.npy'
+featurefile = 'nmecdata/nmec_fiel657_features.npy'
 
 # This is the neural networks and parameters you are deciding to use
-#paramsfile = '/fileserver/iam/iam-processed/models/fiel_657.hdf5'
+paramsfile = '/fileserver/iam/iam-processed/models/fiel_657.hdf5'
 
 
 # ### Parameter Definitions
@@ -79,73 +71,50 @@ lr = 0.001
 
 # ### Define your model
 # 
-print "Compiling model"
-patnet = patnet_layers(num_authors, (1, 56, 56))
+# Here, we're using the Fiel Network
+
+# In[6]:
+vnet = load_verbatimnet( 'fc7', paramsfile=paramsfile, compiling=False )
+vnet.add(Dense(num_authors))
+vnet.add(Activation('softmax'))
+if True:
+  vnet.load_weights('fielnet-iam.hdf5')
 sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
-patnet.compile(loss='categorical_crossentropy', optimizer=sgd)
+vnet.compile(loss='categorical_crossentropy', optimizer=sgd)
 print "Finished compilation"
 
 
+# ### Minibatcher (to load in your data for each batch)
+if False:
+    mini_m = Hdf5MiniBatcher(hdf5authors, num_authors, num_forms_per_author,
+                            shingle_dim=shingle_dim, default_mode=MiniBatcher.TRAIN,
+                            batch_size=load_size, add_rotation=True)
+else:
+    mini_m = IAM_MiniBatcher(hdf5authors, num_authors, num_forms_per_author,
+                            shingle_dim=shingle_dim, default_mode=MiniBatcher.TRAIN,
+                            batch_size=load_size)
+
+
+# ### Train your model for however many specified iterations
 from PIL import Image
 def randangle(batch):
     newbatch = np.zeros(batch.shape)
     for i,im in enumerate(batch):
-        
+        imangle = np.asarray(Image.fromarray(im.squeeze()).rotate(7.5*np.random.randn()))
         newbatch[i]=imangle
     return newbatch
-
-def iam_forms_pre(x, **kwargs):
-    # Chop the top and bottom off
-    x_new = x[700:2700]
-
-    return x_new
-
-def iam_forms_post(x, **kwargs):
-    x_new = 1 - x/255.
-    return x_new
-
-
-# ### Minibatcher (to load in your data for each batch)
-rng = np.random.RandomState(888)
-
-print "Getting Data"
-cc_getter = partial(original_connected.connected_component_getter, 
-                    shingle_dim=shingle_dim,
-                    rng=rng,
-                    fill_value=255,
-                    preprocess_doc=iam_forms_pre,
-                    postprocess=iam_forms_post)
-
-batch_object = Hdf5GetterBatcher(hdf5authors, 
-                     num_authors=num_authors,
-                     num_forms_per_author=num_forms_per_author,
-                     item_getter=cc_getter,
-                     default_mode=MiniBatcher.TRAIN,
-                     batch_size=load_size,
-                     train_pct=.7, test_pct=.2, val_pct=.1,
-                     rng=rng)    
-
-
-# ### Train your model for however many specified iterations
-
-print "Starting training"
 # logging.getLogger('data_iters.hdf5_iterator').setLevel(logging.DEBUG)
-(X_val, Y_val) = batch_object.get_val_batch(1000)
-X_val=X_val.reshape((-1, 1) + shingle_dim)
-Y_val=to_categorical(Y_val,num_authors)
 for batch_iter in range(iterations):
-    t1 = time.time()
-    (X_train,Y_train) = batch_object.get_train_batch()
-    t2 = time.time()
-    print "Retrieved batch {} in {} seconds".format(batch_iter, t2-t1)
-    X_train = X_train.reshape((-1, 1) + shingle_dim)
+    (X_train,Y_train) = mini_m.get_train_batch()
+    X_train = 1.0 - X_train / 255.0
+    X_train = np.expand_dims(X_train, 1)
+    X_train = randangle(X_train)
     Y_train = to_categorical(Y_train, num_authors)
-    patnet.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=1, validation_data=(X_val, Y_val), show_accuracy=True, verbose=1)
+    vnet.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=1, show_accuracy=True, verbose=1)
     print "Finished training on the "+str(batch_iter)+"th batch"
     if (batch_iter % 20)==0 and batch_iter != 0:
-        pass
-        patnet.save_weights('/work/d-script/weights/patnet-iam.hdf5', overwrite=True)
+        vnet.save_weights('fielnet-iam.hdf5', overwrite=True)
 
 
-#patnet.fit(X_train, Y_train, batch_size=32, nb_epoch=1, show_accuracy=True, verbose=1)
+vnet.fit(X_train, Y_train, batch_size=32, nb_epoch=1, show_accuracy=True, verbose=1)
 
